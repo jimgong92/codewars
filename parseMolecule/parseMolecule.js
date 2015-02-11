00b37ceef204de32279129d2740b39f05fa14739
Jimmy Gong <gong.jim@gmail.com>
2015-02-11 11:18:46 -0800

    Finishes parseMolecule.js

--- parseMolecule/parseMolecule.js
+++ parseMolecule/parseMolecule.js
@@ -3,27 +3,34 @@ function parseMolecule(molecule){
   var current = '';
   //Handle multipliers by looking for them immediately
   var multiplier = [1,1,1,1];
-  var inRound = false;
-  var inSquare = false;
-  var inCurly = false;
-
+  var skipIndices = [];
   for (var i = 0; i < molecule.length; i++) {
+    if (skipIndices.indexOf(i) !== -1) continue;
     var ch = molecule[i];
     //Non-bracket number handling
-    if (!isNaN(Number(ch)) && isLetter(ch[i - 1])){
-      multiplier[3] = Number(ch);
+    if (!isNaN(Number(ch)) && isLetter(molecule[i - 1])){
+      if(!isNaN(Number(molecule[i + 1]))){
+        skipIndices.push(i + 1);
+        multiplier[3] = Number(ch + molecule[i + 1]);
+      }
+      else {
+        multiplier[3] = Number(ch);
+      }
       var product = multiplier.reduce(function(p, c){return p * c;}, 1);
       //reset single atom multiplier
       multiplier[3] = 1;
-      atoms[current] = product;
+      atoms[current] = atoms[current] || 0;
+      atoms[current] += product;
+      current = '';
     }
     //Handle uppercase
     if (isLetter(ch)){
-      if (isUpperCase(ch) && i && isNaN(Number(ch[i - 1]))) {
+      if (isUpperCase(ch) && i && isNaN(Number(molecule[i - 1]))) {
         //if not first instance, add current atom to dict
         if (current.length > 0){
           var product = multiplier.reduce(function(p, c){return p * c;}, 1);
-          atoms[current] = product;
+          atoms[current] = atoms[current] || 0;
+          atoms[current] += product;
         }
         //reset current
         current = ch;
@@ -35,47 +42,86 @@ function parseMolecule(molecule){
 
     //Square Bracket Logic Handling
     if (ch === '('){
-      inRound = true;
-      multiplier[1] = getMultiplier(i, ')');
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
+      multiplier[0] = getMultiplier(i, ')');
     }
     if (ch === ')'){
-      inRound = false;
-      multiplier[1] = 1;
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
+      multiplier[0] = 1;
     } 
 
     //Square Bracket Logic Handling
     if (ch === '['){
-      inSquare = true;
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
       multiplier[1] = getMultiplier(i, ']')
     }
     if (ch === ']'){
-      inSquare = false;
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
       multiplier[1] = 1;
     } 
 
     //Curly Bracket Logic Handling
     if (ch === '{'){
-      inCurly = true;
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
       multiplier[2] = getMultiplier(i, '}')
     }
     if (ch === '}'){
-      inCurly = false;
+      if (isLetter(molecule[i - 1])){
+        var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+        atoms[current] = atoms[current] || 0;
+        atoms[current] += product;
+        current = '';
+      }
       multiplier[2] = 1;
     } 
   }
-
+  //Take care of last one
+  if (!atoms[current] && current.length > 0){
+    var product = multiplier.reduce(function(p, c){return p * c;}, 1);
+    atoms[current] = atoms[current] || 0;
+    atoms[current] += product;
+  }
   return atoms;
 
   function getMultiplier(index, bracketType){
     for (var i = index + 1; i < molecule.length; i++){
-      if (molecule[i] === bracketType) return Number(molecule[i + 1]);
+      if (molecule[i] === bracketType) {
+        var multiplier = Number(molecule[i + 1]);
+        if (isNaN(multiplier)) multiplier = 1;
+        return multiplier;
+      }
     }
   }
 }
 function isLetter(ch){
-  var letters = ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return letters.indexOf(ch) >= 0;
 }
 function isUpperCase(ch){
-  return ch.toUpperCase === ch;
+  return ch.toUpperCase() === ch;
 }
