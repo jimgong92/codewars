5b1bdd7e63046c6287dccd0ed8449d10e46a26ef
Jimmy Gong <gong.jim@gmail.com>
2015-02-12 11:42:37 -0800

    Developing functional Lists

---
+++ hamming/hamming.js
@@ -0,0 +1,10 @@
+function hammingFinder(index){
+  var factors = [2, 3 ,5];
+  var move = 0;
+  var res = [1];
+  var trackIndex = 0;
+  while (res.length < index){
+    res.push()
+  }
+  return res[index - 1];
+}
