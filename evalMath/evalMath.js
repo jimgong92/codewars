76e1e72e5dce97b6295d6b6215f463d488157da3
Jimmy Gong <gong.jim@gmail.com>
2015-02-18 18:12:43 -0800

    Scaffold out evaluate math expressions as strings

---
+++ evalMath/evalMath.js
@@ -0,0 +1,3 @@
+function evalMath(str){
+  
+}
---
+++ evalMath/evalMathSpec.js
@@ -0,0 +1,51 @@
+var assert = chai.assert;
+
+describe('Evaluate Mathematical Expressions', function(){
+  describe('Addition', function(){
+    it('should add two numbers', function(){
+      
+    });
+    it('should evaluate negative numbers', function(){
+      
+    });
+    it('should not add broken statements', function(){
+      
+    });
+
+  });
+  describe('Subtraction', function(){
+    it('should subtract two numbers', function(){
+      
+    });
+    it('should evaluate negative numbers', function(){
+      
+    });
+    it('should not subtract broken statements', function(){
+      
+    });
+
+  });
+  describe('Multiplication', function(){
+    it('should multiply two numbers', function(){
+      
+    });
+    it('should evaluate negative numbers', function(){
+      
+    });
+    it('should not multiply broken statements', function(){
+      
+    });
+
+  });
+  describe('Division', function(){
+    it('should divide two numbers', function(){
+      
+    });
+    it('should evaluate negative numbers', function(){
+      
+    });
+    it('should not divide broken statements', function(){
+      
+    });
+  });
+});
